c47f39e0bffcd8d8c0757d7f16f1fca680e24aaf
Aneesh Shastry <aneesh26@gmail.com>
2014-11-02 18:31:36 -0700

    Code updated with comments
    Code updated with comments .
    Removed code for the array.
    Removed un-necesary variables
    Removed all the sysouts

--- Aneesh/Task1_c_d_e.java
+++ Aneesh/Task1_c_d_e.java
@@ -1,27 +1,47 @@
+
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
-
+/**
+ * 
+ * @author Aneesh
+ * The class contains the methods to compare two files and returns a similarity measure
+ *
+ */
 
 public class Task1_c_d_e {
 	
 	private String f1,f2;
 	
-	private static double[][] readWords = new double[15000][10];
 	
-	// Constructor to initialise the string variables with the file names 
-	public Task1_c_d_e(String file1, String file2) {
+	 /**
+	  * 
+	  * @param file1 Input File 1
+	  * @param file2 Input File 2
+	  * @return
+	  * 
+	  * Accepts two files names as arguments. Calls the readFile(String f, ArrayList<String> bag) method 
+	  * for File 1 to read the csv file and collect the words into the arraylist bag1
+	  * Adds all elements from bag1 to the Hashset hs to remove the duplicates from bag1
+	  * Clear bag1 and add all elements from hs to bag1. Now bag1 will contain only distinct words. Clear the hash set hs
+	  * call method readFile using bag2 for File 2 and repeat the steps
+	  * bag1 and bag 2 will have distinct elements from File 1 and File 2
+	  * We need the distinct words from both the files, so we add all the elements from bag1 and bag2 into the hash set hs
+	  * A binary vector for each corresponding to each element in the hash set is formed.
+	  * If the element in the hash set exists in the file, the vector feature is given the value 1, else 0.
+	  * The similarity score is calculated by ratio of number of features with the value 1 to the total number of features in the binary vector. 
+	  * A double values similarity measure is returned by the method
+	  * 
+	  */
+
+	public Double compareFiles(String file1, String file2) {
 		
 		f1 = file1;
 		f2 = file2;
-	}
-	
-	 
-
-	public Double compareFiles() {
 		
 		ArrayList<String> bag = new ArrayList<>();
 		ArrayList<String> bag1 = new ArrayList<>();
@@ -29,23 +49,19 @@ public class Task1_c_d_e {
 		ArrayList<String> bagFile1 = new ArrayList<>();
 		ArrayList<String> bagFile2 = new ArrayList<>();
 		HashSet<String> hs = new HashSet<>();
-		  ArrayList<double[][]> allWords = new ArrayList<>();
-		  
+			  
 		 int simScore = 0; 
 		
 		 readFile(f1,bag); // read file 1 and insert the words into the bag arraylist
 		
-		 bagFile1.addAll(bag);
+		bagFile1.addAll(bag);
 		hs.addAll(bag); // to remove duplicates by adding all words into a hash set
-		for ( String s : hs)
-			System.out.println(s); // To display all the elements in the hash set
-		
 		bag.clear();
 		bag.addAll(hs);
-		allWords.add(readWords); // 
+		 
 		hs.clear();
 		readFile(f2,bag1);
-		allWords.add(readWords);
+		
 		bagFile2.addAll(bag1);
 		hs.addAll(bag1);
 		bag1.clear();
@@ -55,64 +71,35 @@ public class Task1_c_d_e {
 		
 		hs.addAll(bag);
 		hs.addAll(bag1);
-		
-		for ( String s : hs)
-			System.out.println(s);
-		
 		bag2.addAll(hs);
 		
-		
-		
-		
-		System.out.println("Done, Unique lines : " + hs.size());
-		System.out.println("Bag Size :" + bag.size());
-		System.out.println("Bag 1 Size :" + bagFile1.size());
-		System.out.println("Bag 2 Size :" + bagFile2.size());
 		
 		int[][] binary = new int[2][bag2.size()];
 		int[] result = new int[bag2.size()];
 		
-		for ( String s : hs)
-			System.out.println(s);
-		
 			
 		int fcount1 = 0;
 		int fcount2 = 0;
-		for( int i = 0 ; i < bag2.size() ; i++){
+		for( int i = 0 ; i < bag2.size() ; i++)
+		{
 						
-			if (bagFile1.contains(bag2.get(i))){
-			//	System.out.println(bag.indexOf(bagFile1.get(i)));
+			if (bagFile1.contains(bag2.get(i)))
+			{
 				binary[0][i] = 1;
 				fcount1++;
 			}
 			
-			if (bagFile2.contains(bag2.get(i))){
-				//	System.out.println(bag.indexOf(bagFile1.get(i)));
+			if (bagFile2.contains(bag2.get(i)))
+			{
 					binary[1][i] = 1;
 					fcount2++;
-				}
-			
-		}
-		
-		
-		
-		System.out.println();
-		
-		for( int i = 0 ; i < bag2.size() ; i++){
-		 System.out.print(binary[0][i] + " ");
-		}
-		System.out.println();
-		for( int i = 0 ; i < bag2.size() ; i++){
-			 System.out.print(binary[1][i] + " ");
 			}
-		System.out.println();
-		System.out.println("Fcount1 : " + fcount1);
-		System.out.println("Fcount2 : " + fcount2);
-		System.out.println("Bag	: " + bag2.size());
-		//System.out.println(binary[0]);
-		//System.out.println(binary[1]);
 			
-		for( int i = 0 ; i< bag2.size() ; i++ ){
+		}
+		
+			
+		for( int i = 0 ; i< bag2.size() ; i++ )
+		{
 			if(binary[0][i] == binary[1][i]){
 				result[i] = 1;
 				simScore++;
@@ -120,69 +107,49 @@ public class Task1_c_d_e {
 			else{
 				result[i] = 0;
 			}
-			System.out.print(result[i] + " ");
+
 		}
-		System.out.println();
+		double res = (((double)simScore / (double)bag2.size()));
+		return res;
 		
-		//System.out.println(simScore +" " + bag2.size() );
-		double res = (((double)simScore / (double)bag2.size())*100);
-		//res = Math.round(res*100.0)/100.0;
-		//System.out.println("Similarity Score : " + Math.round(res*100.0)/100.0  + "%");
-		return res/100;
-		
-		
-		
-	}	
+	}
+	
+	/**
+	 * 
+	 * @param f The file name
+	 * @param bag The array lost used to collect the words from file
+	 * Accepts the file name as a string and an arraylist to collect the words. Scans through the file
+	 * and extract the words Window vector) line by line and and add them into the arraylist bag.
+	 * 
+	 */
 
 	private static void readFile(String f, ArrayList<String> bag) {
-		// TODO Auto-generated method stub
 		String csvFile = f;
 		BufferedReader br = null;
 		String line = "";
-		String cvsSplitBy = ",";
-		int count = 0;
+		String csvSplitBy = ",";
+		int  count = 0;
 		
-		
-		//readWords = null;
-	 
 		try {
 	 
-			br = new BufferedReader(new FileReader(csvFile));
+			
+				br = new BufferedReader(new FileReader(csvFile));
 			while ((line = br.readLine()) != null) {
-					
-			        // use comma as separator
-				String[] words = line.split(cvsSplitBy);
+				String[] words = line.split(csvSplitBy);
 				String temp = null;
 				for( int i = 3 ; i < words.length ; i++){
-				//System.out.print("<" + words[i].trim() + ">");
-					//System.out.println(">"+words[i]+"<");
-					
 						words[i] = words[i].trim();
-					if (words[i].length() != 0){
-						readWords[count][i-3] = Double.parseDouble(words[i]);// Double.parseDouble(words[i].trim());
-					
-					}
-					else{
-						readWords[count][i-3] = 0.0;
-					
-					}
 
-				//System.out.print(readWords[count][i-3]);
 				if(temp == null)
 					temp = words[i] + ",";
 				else
 					temp = temp + words[i] + ",";
 				
 				}
-
-				//rows.add(cols);
-			//	System.out.println(temp.substring(0, temp.length()-1));
 				temp = temp.substring(0, temp.length()-1);
 				bag.add(temp);
-	//			System.out.println();
-				
-				
 				count++;
+				
 			}
 	 
 		} catch (FileNotFoundException e) {
@@ -198,17 +165,5 @@ public class Task1_c_d_e {
 				}
 			}
 		}
-		//System.out.println("Rows.size : " + rows.size());
-	//	System.out.println("Rows.length : " + rows.get(0).length);
-		
-		
-		
-		
-		
-		
 	  }
-
-
-	
-
 }
