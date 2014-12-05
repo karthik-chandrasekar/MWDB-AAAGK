651420f582b9c2803a76ab6dfc7c1cf1d3ce4cce
Karthik Chandrasekar <karthidec1@gmail.com>
2014-12-05 01:09:46 -0700

    Backup commit

--- Karthik/DecisionTreeClassifier.py
+++ Karthik/DecisionTreeClassifier.py
@@ -1,6 +1,6 @@
-import operator
+import operator, math
 
-class DecisionTreeClassifier
+class DecisionTreeClassifier:
 
     def __init__(self):
         self.features_list = []
@@ -8,6 +8,7 @@ class DecisionTreeClassifier
         self.total_data_entrophy = None
         self.total_data_file_count = 0
         self.top_features_count = 20
+        self.feature_index_to_threshold_map = {}
 
     def get_entrophy(self, data_files_list):
         label_count_hash = {}
@@ -18,30 +19,84 @@ class DecisionTreeClassifier
             count = label_count_hash.get(self.filename_label_hash.get(data_file), 0)
             count += 1
             label_count_hash[data_file] = count
-        
+       
         for key,value in label_count_hash.iteritems():
-            entrophy += (value/files_count) * math.log(value/logcount)
-        
+            entrophy += ((float)(value)/files_count) * math.log((float)(value)/files_count)
+       
         return entrophy
 
     def get_total_data_entrophy(self):
-        data_files_list = []
-        self.total_data_file_count = len(data_files_list)
-        self.total_data_entrophy = self.get_entrophy(data_files_list)
+        object_list = self.index_to_filename_map.values()
+        self.total_data_file_count = len(object_list)
+        self.total_data_entrophy = self.get_entrophy(object_list)
 
-    def load_features_list(self):
-        pass
 
-    def load_object_feature_similarity_matrix(self):
-        pass
+    def load_labels(self):
+        self.labels_input_file = "/tmp/tem/MWDB-AAAGK/Karthik/DecisionTreeInput/labels.csv"
+        self.filename_label_hash = {}
 
-    def self.collect_features(self):
+        with open(self.labels_input_file, 'r') as labels_fd:
+            for line in labels_fd.readlines():
+                if line.startswith('File'):
+                    continue
+                file_name, label = line.strip().split(',')
+                self.filename_label_hash[file_name] = label
+        
+
+    def load_object_feature_matrix(self):
+        self.object_feat_file = "/tmp/tem/MWDB-AAAGK/Karthik/DecisionTreeInput/svdinput.csv"
+        self.object_feat_matrix = []        
+
+        with open(self.object_feat_file, 'r') as object_feat_fd:
+            for line in object_feat_fd.readlines():
+                self.object_feat_matrix.append(line.strip().split(','))
+       
+        self.object_count = len(self.object_feat_matrix)
+        self.feat_count = len(self.object_feat_matrix[0])
+        self.features_list = [x for x in range(self.feat_count)] 
+
+        for i in xrange(self.object_count):
+            for j in xrange(self.feat_count):
+                self.object_feat_matrix[i][j] = int(self.object_feat_matrix[i][j])  
+            
+
+    def load_file_index_map(self):
+        self.file_index_file = "/tmp/tem/MWDB-AAAGK/Karthik/DecisionTreeInput/fileindexmap.csv"
+        self.index_to_filename_map = {}        
+
+        with open(self.file_index_file, 'r') as file_index_fd:
+            for line in file_index_fd.readlines():
+                index, file_name = line.strip().split('-')
+                self.index_to_filename_map[index] = file_name           
+        self.objects_list = self.index_to_filename_map.values()
+ 
+
+    def load_input_data(self):
+        self.load_labels()
+        self.load_object_feature_matrix()
+        self.load_file_index_map()
+    
+    def collect_features(self):
+        self.load_input_data()
         self.get_total_data_entrophy()
-        self.load_features_list()
-        self.load_object_feature_similarity_matrix()
 
     def get_pruned_data(self, feature):
-        //reutrn files that has feature tf value greater than threshold [Median]
+        #reutrn files that has feature tf value greater than threshold [Median]
+       
+        tf_value_list = []
+        output_list = []       
+ 
+        for i in xrange(self.object_count):
+            tf_value_list.append(self.object_feat_matrix[i][feature])  
+        
+        threshold = (float)(sum(tf_value_list))/len(tf_value_list)         
+
+        for i in xrange(self.object_count):
+            if self.object_feat_matrix[i][feature] > threshold:
+                   output_list.append(i)
+
+        self.feature_index_to_threshold_map[feature] = threshold
+        return output_list
 
     def get_gain_rhs(self, feature):
         pruned_file_list = self.get_pruned_data(feature)
@@ -53,116 +108,48 @@ class DecisionTreeClassifier
         return self.total_data_entrophy - self.get_gain_rhs(feature)
 
     def get_feature_importance(self):
+        self.feature_to_gain_value = {}
+        
         for feature in self.features_list:
             self.feature_to_gain_value[feature] = self.get_gain_value(feature)
+
 
     def get_important_features(self, k):
         self.collect_features()
         self.get_feature_importance()
-        important_feature_tuple_list = sorted(self.feature_to_gain_value.items(), key=operator.itemgetter(1))[k]
+        important_feature_tuple_list = sorted(self.feature_to_gain_value.items(), key=operator.itemgetter(1), reverse=True)[:k]
         important_features_list = []        
 
-        for key_value in important_features_list:
+        for key_value in important_feature_tuple_list:
             important_features_list.append(key_value[1]) 
 
         return important_features_list
-
-    def form_tree(self, features_list):
-        
-        root = dict()
-        for index, feature in features_list:   
-            l_val_list, r_val_list = self.get_pruned_data_partitions(feature)
-            
-            l_current_dict = root.setdefault(str(index)+'-left', {})
-            r_current_dict = root.setdefault(str(index)+'right', {})
-
-            is_l_homogenous, label = self.data_list_homogenous(l_val_list)
-            if is_l_homogenous:
-                l_current_dict['label'] = label
-
-            is_r_homogenous, label = self.data_list_homogenous(r_val_list)
-            if is_r_homogenous:
-                r_current_dict['label'] = label
-
-            l_current_dict['l_val'] = l_val_list
-            r_current_dict['r_val'] = r_val_list
-            l_current_dict['l_term'] = is_l_homogenous
-            r_current_dict['r_term'] = is_r_homogenous
-
-            if (is_l_homogenous && is_r_homogenous):
-                break
-
-
-    def form_tree(self, feature_list, feature_index, tree_dict, homogenous):
-        if homogenous:return
-
-        val_list = tree_dict.setdefault(feature, [])
-              
-        
-
-            
-    def form_tree(self, feature_list, feature_index, tree_dict, l_homogenous, r_homogenous):
-        if l_homogenous && r_homogenous:
-            return 
-
-        if not l_homogenous:
-            l_val_list = tree_dict.get('l_val_list', [])
-            ll_val_list, lr_val_list = self.get_pruned_data_partitions(feature_list.get(index), l_val_list)
-            l_current_dict = tree_dict.setdefault(feature+"-left", {})
-            
-            l_homogenous, label = self.is_data_list_homogenous(ll_val_list)
-            r_homogenous, label = self.is_data_list_homogenous(lr_val_list)
-
-            l_current_dict['l_val_list'] = ll_val_list
-            if l_homogenous:
-                l_current_dict['label'] = label
-           
-            l_current_dict['r_val_list'] = lr_val_list
-            if r_homogenous:
-                r_current_dict['label'] = label
- 
-            form_tree(feature_list, index+1, l_current_dict, l_homogenous, r_homogenous)
-            form_tree(feature_list, index+1, r_current_dict, l_homogenous, r_homogenous)
-            
-                        
-
-        if not r_homogenous:
-            r_val_list = tree_dict.get('r_val_list', [])
-            rl_val_list, rr_val_list = self.get_pruned_data_partitions(feature_list.get(index), r_val_list)
-            r_current_dict = tree_dict.setdefautl(feature+"-right", {})
-
-            l_homogenous, label = self.is_data_list_homogenous(rl_val_list)
-            r_homogenous, label = self.is_data_list_homogenous(rr_val_list)
-
-            r_current_dict['l_val_list'] = rl_val_list
-            if l_homogenous:
-                r_current_dict['label'] = label
                 
-            r_current_dict['r_val_list'] = rr_val_list
-            
-             
-                
+    def form_tree(self, features_list, index, key, objects_list, tree_map):
+        is_homogenous, label = self.check_homogenity(objects_list)
 
+        if label:
+            tree_map[key] = label
+            tree_map['leaf'] = True
+            return
 
-def make_trie(*words):
-...     root = dict()
-...     for word in words:
-...         current_dict = root
-...         for letter in word:
-...             current_dict = current_dict.setdefault(letter, {})
-...         current_dict = current_dict.setdefault(_end, _end)
-...     return root
-
+        true_objects_list, false_objects_list = self.split_objects(objects_list, features_list, index)
+        new_map = tree_map.setdefault(key, {})
+        form_tree(features_list, index+1, str(index)+'P', true_objects_list, new_map)
+        form_tree(features_list, index+1, str(index)+'N', false_objects_list, new_map)
+        return tree_map 
 
     def training_phase(self):
         important_features_list = self.get_important_features(self.top_features_count)
-        self.form_tree(important_features_list)
+        tree_map = {}
+        import pdb;pdb.set_trace()
+        decision_tree_map = self.form_tree(important_features_list, 0, 'R', self.objects_list, tree_map)
 
     def classifying_phase(self):
         self.classifiy()        
     
     def main(self):
-        self.training_phase(self)
+        self.training_phase()
         self.classifying_phase()
 
 if __name__ == "__main__":
