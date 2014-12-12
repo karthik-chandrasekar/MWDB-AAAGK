efc32a6663a1cdf048e16c5277a94e5c820babe8
anilkuncham@gmail.com <anilkuncham@gmail.com>
2014-12-12 01:40:52 -0700

    fix for file index map

---
+++ Anil/phase3/pagerank_task3c_new.m
@@ -0,0 +1,96 @@
+function pagerank()
+
+prompt1 = 'Enter simulation similarity graph path: ';
+fname = input(prompt1,'s');
+prompt2 = 'Enter k value: ';
+k = input(prompt2,'s');
+k = str2num(k);
+prompt3 = 'Enter c value: ';
+c = input(prompt3,'s');
+c = str2num(c);
+prompt4 = 'Enter simulaton file 1:';
+f1 = input(prompt4,'s');
+f1 = str2num(f1);
+prompt5 = 'Enter simulaton file 2:';
+f2 = input(prompt5,'s');
+f2 = str2num(f2);
+
+
+A = csvread(fname);
+% A = csvread('AdjMatrix.csv');
+M = transpose(A);
+[rows, cols] = size(M)
+for i=1:cols
+    s = sum(M(:,i));
+    if s ~= 0
+        M(:,i)=M(:,i)./s;
+    end;
+end;
+
+
+K = zeros(rows,1);
+
+IndexMatrix = csvread('pgfileindexmap.csv');
+[idxrowSize,idxcolSize] = size(IndexMatrix);
+for e=1:idxrowSize
+    if(IndexMatrix(e,2) == f1)
+        K(e,1) = 0.5;
+    end;
+    if(IndexMatrix(e,2) == f2)
+        K(e,1) = 0.5;
+    end;
+end;
+
+I = eye(rows,cols);
+
+
+cont = 1;
+Rinitial = zeros(rows,1);
+Rfinal = zeros(rows,1);
+Rinitial = K;
+size(Rinitial)
+i=1;
+match_count = 0;
+while cont
+    disp('iter');
+    disp(i);
+    Rfinal = (1 - c).*M*Rinitial + c.*K;
+%     for j=1:rows
+%         if round(Rinit(j,1),5) == round(Rfinal(j,1),5)
+%             match_count = match_count + 1;
+%         end;
+%     end;
+%     if match_count == rows
+%         cont = false;
+%     end;
+    if (isequal(round(Rinitial*1000)/1000000,round(Rfinal*1000)/1000000))
+%       if isequal(Rinit,Rfinal)
+        cont = false;
+     end;
+    Rinitial = Rfinal;
+    i=i+1;
+end;
+
+[output,index] = sort(Rfinal,'descend');
+[orows,ocols] = size(output);
+fprintf('Top %d page rank files are: \n',k);
+for m=1:k
+    fprintf('File name %d.csv -> page rank %f \n',IndexMatrix(index(m,1),2),output(m,1));
+end;
+% final_output = [];
+% temp1 = [];
+% temp2 = [];
+% for f=1:ocols
+%     temp1(f) = output(1,f);
+%     temp2(f) = index(1,f);
+% end;
+% final_output = horzcat(temp1(1:k),temp2(1:k))
+% disp(Sim);
+
+
+
+
+
+
+
+end
